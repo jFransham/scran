6b044e1cfdcb748a43185b983ab98a6eae3b1003
Jack Fransham <moonfudgeman@hotmail.co.uk>
2017-04-06 12:25:29 +0200

    Racketify

--- scran/macros.rkt
+++ scran/macros.rkt
@@ -1,7 +1,10 @@
+#lang r5rs
+
 ;;; Gambit-c requires that macro-code be `included` directly into
 ;;; source code that uses it.  Hence, we separate the macro code from
 ;;; the main body of the scran so that the main body of the code
 ;;; doesn't need to be endlessly recompiled.
 
-(define-macro (define-component name args #!rest body)
+;; TODO: Racketify this
+(define-macro (define-component name args . body)
   `(define ,name (component! (lambda ,args ,@body))))
